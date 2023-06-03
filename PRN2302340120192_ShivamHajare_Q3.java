edbcbd65817835b1975982f44a31bdbd11d02ca2
Shivam Manohar Hajare <87773897+Shivam-Hajare@users.noreply.github.com>
2023-06-03 18:02:37 +0000

    sop added

--- PRN2302340120192_ShivamHajare_Q3.java
+++ PRN2302340120192_ShivamHajare_Q3.java
@@ -1,5 +1,6 @@
 public class PRN2302340120192_ShivamHajare_Q3 {
         public static void main(String[] args) {
+            System.out.println("hi")
             int flag=0;
             System.out.println("1");
             for(int i=1;i<=100;i++)
